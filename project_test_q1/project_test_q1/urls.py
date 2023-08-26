f81adbfad330f1140b6208a16822a0b885dab611
bemarchant <bemarchant@uc.cl>
2023-08-26 14:46:58 -0400

    fix(app): import views

--- project_test_q1/project_test_q1/urls.py
+++ project_test_q1/project_test_q1/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from app_test_q1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
